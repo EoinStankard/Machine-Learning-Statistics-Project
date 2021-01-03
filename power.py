d672551e73d5461fff4820a8b66a1dccbc421b19
Eoin Stankard <G00300390@gmit.ie>
2021-01-03 20:48:52 +0000

    Updated HTML and server

--- power.py
+++ power.py
@@ -17,17 +17,25 @@ def home():
   return app.send_static_file('power.html')
 
 # Add uniform route.
-@app.route('/api/predict')
-def turbinePower():
-  df = pd.read_csv("powerproduction.csv")
-  inputs_train, inputs_test, outputs_train, outputs_test = mod.train_test_split(df['speed'], df['power'], test_size=0.2)
-  model = kr.models.Sequential()
-  model.add(kr.layers.Dense(50, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
-  model.add(kr.layers.Dense(1, activation='linear', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
-  model.compile(kr.optimizers.Adam(lr=0.001), loss='mean_squared_error')
-  model.fit(inputs_train, outputs_train, epochs=500)
-  print("%.2f" %model.predict([5]))
-  s = float(model.predict([8.238]))
-  print(s)
-  #np.random.uniform()
-  return {"value": s}
+@app.route('/api/predict/<speed>')
+def turbinePower(speed):
+    modelSaved = kr.models.load_model('powerProductionModel.h5')
+    #print("£££££££££££££££££££££££££  SPEED: ", float(speed))
+    ss = float(speed)
+    print("SPEED: ",ss)
+    s = float(modelSaved.predict([ss]))
+    print("PREDICTED: ",s)
+    return {"value": s}
+
+def createModel():
+    df = pd.read_csv("powerproduction.csv")
+    inputs_train, inputs_test, outputs_train, outputs_test = mod.train_test_split(df['speed'], df['power'], test_size=0.2)
+    model = kr.models.Sequential()
+    model.add(kr.layers.Dense(50, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+    model.add(kr.layers.Dense(1, activation='linear', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+    model.compile(kr.optimizers.Adam(lr=0.001), loss='mean_squared_error')
+    model.fit(inputs_train, outputs_train, epochs=500)
+    model.save('powerProductionModel.h5')
+    s = float(model.predict([8.238]))
+
+#createModel()
